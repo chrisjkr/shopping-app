1ab062e128abfa5fd3b712746d122dd6adbed534
Krzysztof Kraszewski <krzysztof@kraszewski.app>
2018-07-27 13:16:34 +0200

    feat: set static shopping list item height
    Signed-off-by: Krzysztof Kraszewski <krzysztof@kraszewski.app>

--- src/components/ShoppingListItem/styles.js
+++ src/components/ShoppingListItem/styles.js
@@ -2,8 +2,10 @@ import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
   card: {
+    height: 55,
     flexDirection: 'row',
     padding: 15,
+    alignItems: 'center',
   },
 
   checkedCard: {
