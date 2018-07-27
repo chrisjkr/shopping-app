ccbdfa923a17ea26a783659e2dd5d647c45bf137
Krzysztof Kraszewski <krzysztof@kraszewski.app>
2018-07-28 01:14:18 +0200

    feat: add big quantity message with animation
    Signed-off-by: Krzysztof Kraszewski <krzysztof@kraszewski.app>

--- src/screens/NewItem/index.js
+++ src/screens/NewItem/index.js
@@ -11,7 +11,7 @@ import {
   Item,
   Label,
 } from 'native-base'
-import { Dimensions } from 'react-native'
+import { Dimensions, Animated } from 'react-native'
 import styles from './styles'
 import ToastService from '../../services/ToastService'
 import FormScreenComponent from '../../components/FormScreenComponent'
@@ -40,33 +40,63 @@ export default class NewItem extends FormScreenComponent<void, State> {
     width: '98%',
   }
 
+  quantityMessageOpacity: Animated.Value = new Animated.Value(0)
+
   get listId(): string {
     return this.navigation.state.params.listId
+  }
+
+  showQuantityMessage() {
+    Animated.timing(this.quantityMessageOpacity, {
+      toValue: 1,
+      duration: 500,
+    }).start()
+  }
+
+  hideQuantityMessage() {
+    Animated.timing(this.quantityMessageOpacity, {
+      toValue: 0,
+      duration: 500,
+    }).start()
+  }
+
+  showBiggerQuantityMessage = (value: string) => {
+    if (value.length > 2) {
+      this.showQuantityMessage()
+    } else {
+      console.log('hide')
+      this.hideQuantityMessage()
+    }
   }
 
   onAddPress = () => {
     const { itemName, quantity } = this.state
     if (itemName.length === 0) {
-      ToastService.show({ text: 'Please name your item', style: this.toastStyle })
+      ToastService.show({
+        text: 'Please name your item',
+        style: this.toastStyle,
+      })
       return
     }
     console.log(quantity.length === 0)
     if (quantity.length === 0) {
-      ToastService.show({ text: 'Please insert item quantity', style: this.toastStyle })
+      ToastService.show({
+        text: 'Please insert item quantity',
+        style: this.toastStyle,
+      })
       return
     }
 
     if (isNaN(quantity)) {
-      ToastService.show({ text: 'Invalid quantity format', style: this.toastStyle })
+      ToastService.show({
+        text: 'Invalid quantity format',
+        style: this.toastStyle,
+      })
       this.setState({ quantity: '1' })
       return
     }
 
-    this.store.addListItem(
-      itemName,
-      Number(quantity),
-      this.listId,
-    )
+    this.store.addListItem(itemName, Number(quantity), this.listId)
     ToastService.show({ text: 'Item added!' })
     this.navigation.goBack()
   }
@@ -85,11 +115,23 @@ export default class NewItem extends FormScreenComponent<void, State> {
           <Item floatingLabel style={styles.item}>
             <Label>Quantity</Label>
             <Input
-              keyboardType='numeric'
+              keyboardType="numeric"
               value={this.state.quantity}
-              onChangeText={this.handleInputChange('quantity')}
+              onChangeText={value => {
+                this.handleInputChange('quantity')(value)
+                this.showBiggerQuantityMessage(value)
+              }}
             />
           </Item>
+          <Animated.Text
+            style={{
+              color: 'gray',
+              opacity: this.quantityMessageOpacity,
+            }}
+          >
+            Are you actually adding {this.state.quantity} of{' '}
+            {this.state.itemName.length ? this.state.itemName : 'nothing'}?
+          </Animated.Text>
         </Content>
         <Button full style={styles.addButton} onPress={this.onAddPress}>
           <Text>Add</Text>
